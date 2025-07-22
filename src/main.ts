cd918fe5119f53d63bb16c9ee5ac105739e07092
phrezende-eng <ph.hr.001@gmail.com>
2025-07-22 02:49:35 -0300

    chore: new params for k6s

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,19 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
 
-  console.log("App running at 3001")
+  app.use((req, res, next) => {
+    res.setHeader('Connection', 'keep-alive');
+    next();
+  });
+
+  await app.listen(process.env.PORT ?? 3001);
+  console.log("App running at 3001");
 
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
-    console.log("Hot reload activated!")
+    console.log("Hot reload activated!");
   }
 }
 bootstrap();
-
